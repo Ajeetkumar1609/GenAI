8626c5819c4ae3b3d397a7562e7f6c03ea4a08dc
Ajeet Kumar <itsajeetkumar.dev@gmail.com>
2026-09-14 20:13:02 +0530

    Prepare backend for deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,10 +8,19 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true
+// }))
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        process.env.FRONTEND_URL
+    ].filter(Boolean),
     credentials: true
-}))
+}));
 
 /* using all the routes here */
 app.use("/api/auth", authRouter);
